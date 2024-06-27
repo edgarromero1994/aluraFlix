a8b1a09408b4aae725ccf3bef32c4d389b7f2f1c
edgarromero1994 <edgarromerocuc94@gmail.com>
2024-06-27 11:37:34 -0600

    actualizacion del codigo

--- src/components/videodestacado/VideoDestacado.jsx
+++ src/components/videodestacado/VideoDestacado.jsx
@@ -1,13 +1,43 @@
+import { useState } from "react";
 import "./VideoDestacado.css"
+import ReactPlayer from 'react-player';
 
 const VideoDestacado = ({video}) => {
+  const [playing, setPlaying] = useState(false);
+  const [currentPlayingId, setCurrentPlayingId] = useState(null); 
+
+   const handlePlayVideo = () => {
+    if (video.id === currentPlayingId && playing) {
+      setPlaying(false);  
+    } else {
+      setCurrentPlayingId(video.id);  
+      setPlaying(true);  
+    }
+  };
+
   return (
     <div className="card-destacado">
-      <img src={video.imagen} alt={video.titulo} />
-      <div className="details">
-        <span>{video.visualizacion}</span>
-        <h6>{video.titulo}</h6>
-      </div>
+      {!playing ? (
+      <img
+        src={video.imagen}
+        alt={video.titulo}
+        onClick={handlePlayVideo}
+      />
+    ) : (
+      <ReactPlayer
+        url={video.video}
+        width="100%"
+        height="100%"
+        controls={true}
+        playing={playing}
+        onEnded={() => setPlaying(false)} // Pausa el video al finalizar la reproducción
+      />
+    )}
+    <div className="details">
+      <span>{video.visualizacion}</span>
+      <h6>{video.titulo}</h6>
+
+    </div>
     </div>
   )
 }
